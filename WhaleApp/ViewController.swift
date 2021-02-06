8b8895b787125b71b86ffb7b2423cf062766a684
Horacio Alexandro <horacio@swoopin.net>
2021-02-06 14:21:07 -0800

    Initial Commit

---
+++ WhaleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhaleApp
+//
+//  Created by Horacio Alexandro Sanchez on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
